e37b9cfbe8f536f6b6692c18a9d1b8ac4fe6db6c
madman <143639303+Shubham151104@users.noreply.github.com>
2023-12-13 22:44:42 +0530

    Update and rename oop 3 to oop3.cpp

--- oop 3.cpp
+++ oop 3.cpp
@@ -1,10 +1,4 @@
-/*       PROBLEM STATEMENT
---Imagine a publishing company which does marketing for book and audiocassette versions.
-Create a class publication that stores the title (a string) and price (type float) of a publication.
-From this class derive two classes: book, which adds a page count (type int), and tape, which adds a playing time in minutes (type float).
-Write a program that instantiates the book and tape classes, allows user to enter data and displays the data members. If an exception is caught, replace all the data member values with
-zero values.
-*/
+
 
 
 #include<iostream>
