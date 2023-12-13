81e1379006a661ea384b59cffd039a8a698545dc
madman <143639303+Shubham151104@users.noreply.github.com>
2023-12-13 22:45:24 +0530

    Update and rename oop 4 to oop4.cpp

--- oop4.cpp
+++ oop4.cpp
@@ -1,8 +1,4 @@
-/*      PROBLEM STATEMENT
- Write a C++ program that creates an output file, writes information to it, 
- closes the file and open it again as an input file and read the information 
- from the file.
-*/
+
 #include<iostream>
 #include<fstream>
 using namespace std;
